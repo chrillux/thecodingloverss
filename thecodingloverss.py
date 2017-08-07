5b84217e97e4936fe887368cdaf134ce1f9eab88
Christian Biamont <christianbiamont@gmail.com>
2017-08-07 21:00:19 +0200

    The original The Coding Love now has images in RSS
    This application is no longer needed thanks to the guys at thecodinglove.com.
    Their RSS feed now includes images!

--- thecodingloverss.py
+++ thecodingloverss.py
@@ -1,6 +1,5 @@
-import feedparser, sys, urllib2
-
-from bs4 import BeautifulSoup as BS
+import datetime
+import pytz
 from feedgen.feed import FeedGenerator
 from flask import Flask
 
@@ -9,38 +8,24 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 
-    d = feedparser.parse('http://thecodinglove.com/rss')
+    thecodingloveurl = "http://thecodinglove.com/rss"
+    pubdate = datetime.datetime(2017, 8, 7, 21, 0, 0, 0, pytz.UTC)
 
     fg = FeedGenerator()
     fg.title('The coding love with images.')
-    fg.link(href='http://thecodinglove.com')
+    fg.link(href=thecodingloveurl)
     fg.description('The coding love with images.')
 
-    for entry in d.entries:
-
-        try:
-            contributor = entry.summary_detail.value
-        except:
-            contributor = "n/a"
-
-        href = entry.links[0].href
-        published = entry.published
-        title = entry.title
-
-        bs = BS(urllib2.urlopen(href), "lxml")
-        image = bs.p.img.get('src')
-        imgsrc='<img src="%s">' % image
-
-        description = "%s <br/> %s" % (imgsrc, contributor)
-
-        fe = fg.add_entry()
-
-        fe.id(href)
-        fe.link(href=href)
-        fe.pubdate(published)
-        fe.title(title)
-        fe.description(description)
+    fe = fg.add_entry()
+    fe.id("Life is good.")
+    fe.link(href=thecodingloveurl)
+    fe.title("The original coding love now has images!")
+    fe.pubdate(pubdate)
 
     rssfeed  = fg.rss_str(pretty=True)
+
     return rssfeed
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
 
